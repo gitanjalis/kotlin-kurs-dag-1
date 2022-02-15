d185a49d90b670f5d2e690d22333f2a5c96f1be3
August Lilleaas <august@augustl.com>
2022-02-15 14:40:59 +0100

    Disentangle handlers from ktor

--- src/main/kotlin/kurstest/routes.kt
+++ src/main/kotlin/kurstest/routes.kt
@@ -8,6 +8,7 @@ import io.ktor.http.content.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.util.pipeline.*
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
@@ -16,11 +17,19 @@ import kotlinx.coroutines.withTimeout
 import kotlinx.html.*
 import kotliquery.Session
 import kotliquery.queryOf
+import kotliquery.sessionOf
 import javax.sql.DataSource
 import kotlin.concurrent.thread
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
+
+sealed class WebResponse(
+    val statusCode: HttpStatusCode = HttpStatusCode.OK,
+    val headers: Map<String, String> = mapOf()
+)
+data class HtmlResponse(val body: Template<HTML>) : WebResponse()
+data class TextResponse(val body: String) : WebResponse()
 
 fun Application.createKursKtorApplication(appConfig: AppConfig, dataSource: DataSource) {
     install(StatusPages) {
@@ -43,7 +52,7 @@ fun Application.createKursKtorApplication(appConfig: AppConfig, dataSource: Data
             }
         }
 
-        get("/", withDbSession(dataSource, ApplicationCall::handleHomePage))
+        get("/", webResponseWithDbSession(dataSource, ::handleHomePage))
 
         get("/about") { call.handleAboutPage() }
 
@@ -61,15 +70,36 @@ fun Application.createKursKtorApplication(appConfig: AppConfig, dataSource: Data
     }
 }
 
-suspend fun ApplicationCall.handleHomePage(dbSess: Session): Unit {
+fun webResponseWithDbSession(dataSource: DataSource, body: (dbSess: Session) -> WebResponse): PipelineInterceptor<Unit, ApplicationCall> {
+    return {
+        val webResp = sessionOf(dataSource, returnGeneratedKey = true).use { body(it) }
+        for ((key, value) in webResp.headers) {
+            call.response.header(key, value)
+        }
+
+        when (webResp) {
+            is HtmlResponse -> {
+                call.respondHtml(status = webResp.statusCode) {
+                    with(webResp.body) { apply() }
+                }
+            }
+            is TextResponse -> {
+                call.respondText(status = webResp.statusCode, text = webResp.body)
+            }
+        }
+    }
+}
+
+fun handleHomePage(dbSess: Session): WebResponse {
     val row = dbSess.single(queryOf("SELECT count(*) FROM user_t"), ::mapFromRow)
-    respondHtmlTemplate(KursLayout()) {
+
+    return HtmlResponse(KursLayout().apply {
         pageBody {
             h1 { +"Hei kurs!" }
 
             p { +"Databasen sier: ${row}" }
         }
-    }
+    })
 }
 
 suspend fun ApplicationCall.handleAboutPage() {
